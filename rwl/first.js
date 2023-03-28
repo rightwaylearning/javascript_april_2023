14e84c299026dbe81be8225d2352a16d6967e1f7
right way learning <java.development.pune@gmail.com>
2023-03-28 10:09:39 +0530

    data

---
+++ rwl/first.js
@@ -0,0 +1,6 @@
+
+let fiartName = "mahesh";
+let age = 23;
+
+console.log(fiartName);
+console.log(age);
---
+++ rwl/second.js
@@ -0,0 +1,27 @@
+
+let age =23;
+
+let fullName= "ajit sharma";
+
+let flag = false;         // boolean have only 2 values i.e true & false
+
+
+let k = undefined;       // undefined is data type having value undefined;
+
+// default value od variable is undefined
+
+let lastName;
+
+console.log("lastName = "+lastName);
+console.log(" k ="+k)
+
+// number,string,boolean,undefined
+
+// null is data type having only one value that is null itself
+
+let marks = null;
+
+console.log("marks = "+marks);
+
+
+
