73f3a40e8afb0cbca6ec50e02e89deaa21749f04
Your Name <you@example.com>
2023-03-28 19:40:41 +0530

    javascript prgm

---
+++ Prachi_Pawar/dyanamic.js
@@ -0,0 +1,15 @@
+let a = 12;
+
+console.log(typeof 100);
+
+a="hello";
+console.log(typeof a);
+
+a= true;
+console.log(typeof a);
+
+a= undefined;
+console.log(typeof a);
+
+a=null;
+console.log(typeof a);
---
+++ Prachi_Pawar/first.js
@@ -0,0 +1,5 @@
+let fiartName="Akash";
+let age = 25;
+
+console.log(fiartName);
+console.log(age);
