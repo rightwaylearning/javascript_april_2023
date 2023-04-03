3f4fcdaa223bd66f7896adbceb624caad61aa35b
right way learning <java.development.pune@gmail.com>
2023-04-03 08:18:05 +0530

    data

---
+++ rwl/03-04-2023/function4.js
@@ -0,0 +1,15 @@
+
+let obj={
+    name:"Sushant",
+    age:38,
+    character:function(str){
+        console.log(str);
+    }
+}
+
+console.log(obj.name);
+console.log(obj.age);
+// console is object & log is methodm of that object
+
+obj.character("Sushant is good actor");
+
