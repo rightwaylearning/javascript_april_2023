88baaad9eb0cbc0182754161c3fabd77d29a69a6
abc <swapnilplan@gmail.com>
2023-04-03 18:28:54 +0530

    data

---
+++ Swapnil_Vyavahare/03-04-2023/loop.js
@@ -0,0 +1,27 @@
+let arr= [10,20,30,40,50];
+
+// for(let i=0; i<arr.length;i++){
+//     console.log(arr[i])
+// }
+
+
+// let i =0;
+// while(i<arr.length){
+//     console.log(arr[i])
+//     i++
+// }
+
+
+
+let foo= function(i){
+    console.log(i);
+}
+
+arr.forEach(foo);
+
+
+
+
+
+
+
---
+++ Swapnil_Vyavahare/31-03-2023/fun_return.js
@@ -0,0 +1,19 @@
+function sum(){
+    console.log("Hi hello")
+    return 100;
+}
+
+let a=sum();
+console.log(a)
+
+
+function sum(){
+    console.log("Hi hello")
+    
+}
+
+
+sum();
+sum();
+sum();
+sum();
