2209cd36519e03317708d303a9662b1e115486f3
abc <swapnilplan@gmail.com>
2023-04-04 18:21:56 +0530

    hi

---
+++ Swapnil_Vyavahare/04-04-2023/first.js
@@ -0,0 +1,25 @@
+// let obj=[
+//        "swapnil",
+//        "vyavahare",
+//        38,
+//        35000
+// ]
+
+// obj['length'] =35
+
+
+
+// console.log(obj['length'])
+
+
+obj1['length'] = 3;
+let obj1 = [
+    "sukesh",
+    38,
+    "TN",
+    "south india"    
+];
+
+
+
+console.log(obj1['length']); // 4
