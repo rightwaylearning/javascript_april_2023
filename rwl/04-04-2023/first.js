f7d3243cdbb01e74a160a35a73c56af02627d052
right way learning <java.development.pune@gmail.com>
2023-04-04 08:41:29 +0530

    fdata

---
+++ rwl/04-04-2023/first.js
@@ -0,0 +1,44 @@
+let obj = {
+    "0":"sukesh",
+    "1":38,
+    "2":"TN",
+    "3":"from south india",
+    length:4
+};
+
+for(let i = 0 ;i < obj.length ; i++){
+   console.log(obj[i]);
+}
+
+
+//obj['fullName'] = "Sukesh reddy";
+// variable names , obj >> name,age,from
+
+// console.log(obj[0]);
+// console.log(obj[1]);
+// console.log(obj[2]);
+// console.log(typeof obj);
+// obj['length'] = 200;
+// console.log(obj.length);
+
+//----------------------------------------------------------------------------------
+
+let obj1 = [
+    "sukesh",
+    38,
+    "TN",
+    "south india"    
+];
+obj1['length'] = 35;
+
+
+console.log(obj1['length']); // 4
+
+
+
+
+
+
+
+
+
