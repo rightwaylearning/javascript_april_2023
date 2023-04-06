4118b79bd7f449cdba5696a60a7cdccd9871fed9
Your Name <you@example.com>
2023-04-06 12:42:33 +0530

    create a program

---
+++ Prachi_Pawar/03-04-2023/function1.js
@@ -0,0 +1,28 @@
+
+function test(){
+    //message
+}
+
+test();
+
+let m=function(){
+    //logic
+}
+m();
+//function expression
+
+function doSomething(){
+    console.log(a+b);
+}
+
+function doSomething(a,b){
+    console.log(a+b); 
+}
+
+let p1=10;
+let p2=30;
+
+doSomething(p1,p2);
+
+//-----------------------------------------
+
