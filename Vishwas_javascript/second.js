7c070f4d2e53fc5d08b188020725b26fce2a02ed
Vishwas <vishpayan04@gmail.com>
2023-04-11 12:58:36 +0530

    data

---
+++ Vishwas_javascript/second.js
@@ -0,0 +1,13 @@
+let firstName = "kajal";
+let lastname = "Patil";
+console.log(firstName);
+
+let num = 90.111;
+let name = "Rohit Sharma";
+let flag = true;    //in javaSript boolean have only two value True and False
+console.log(flag);
+let marks = null;
+console.log("marks==  "+marks);
+
+// number, String, boolean,undefined,null,
+//object, array,function,BigInt,symbol
