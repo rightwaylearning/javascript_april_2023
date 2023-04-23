a9ac397618eae832b29c9d04202d91c264f1799b
right way learning <java.development.pune@gmail.com>
2023-04-23 20:22:55 +0530

    data

---
+++ rwl_dom/06-04-2023/23-04-2023/myLogic.js
@@ -0,0 +1,19 @@
+document.getElementById("bt1").addEventListener("click",function(){
+    console.log(event);
+    let msg = document.getElementById("text1").value; 
+    
+    if(msg.length == 0){
+        alert("Please !! enter some message");
+    }else{
+        let str = location.href;
+        if(str.includes("first")){
+            document.getElementById("id1").innerHTML = msg;
+        }else{
+            document.getElementById("id1").innerHTML = "Message "+msg;
+        }
+    }
+ })
+
+ document.getElementById("bt2").addEventListener("click",function(){
+     document.getElementById("text1").value ='';
+ });
